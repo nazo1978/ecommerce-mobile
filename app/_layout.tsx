5d206a380f73e048ccd5925b2f25a7c05c41f945
ege <emrevaryemez@gmail.com>
2025-06-03 15:54:04 +0300

    ürünler sayfası hem web hemde mobilde düzeltildi

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,30 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import React from 'react'
 import { Stack } from 'expo-router'
 
-export default function RootLayout(){
-  return <GluestackUIProvider mode="light"><Stack/></GluestackUIProvider>;
+export default function RootLayout() {
+  return <GluestackUIProvider>
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="product/[id]"
+        options={{
+          title: "Product Details",
+          headerShown: true,
+        }}
+      />
+      <Stack.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          headerShown: true,
+        }}
+      />
+    </Stack>
+    </GluestackUIProvider>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,32 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import products from "../assets/products.json"
 import { ProductListItem } from '../components/ProductListItem';
+import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
 
 
-export default function HomeScreen(){
+
+
+export default function HomeScreen() {
+    //const { width } = useWindowDimensions();
+    //const numColumns = width > 768 ? 3 : 2;
+
+    const numColumns = useBreakpointValue({ 
+            default: 2, 
+            sm: 3 ,
+            xl: 4
+        }
+    );
+
     return (
-
-        <View>
-            <FlatList 
+        <FlatList
             data={products}
-            numColumns={2}
-            contentContainerClassName='gap-2'
+            numColumns={numColumns}
+            key={numColumns}
+            contentContainerClassName='gap-2 max-w-[960px] mx-auto w-full'
             columnWrapperClassName='gap-2'
-            renderItem={({item}) => 
-            <ProductListItem product={item} />}     
-             />
-        </View>
+            renderItem={({ item }) =>
+                <ProductListItem product={item} />}
+        />
+
     );
 }
