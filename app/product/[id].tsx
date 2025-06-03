faee1abcd683718ae62855a19bd88e0a387ee8cf
ege <emrevaryemez@gmail.com>
2025-06-03 15:10:47 +0300

    product detail screen

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,14 +1,58 @@
 import { useLocalSearchParams } from 'expo-router';
 import { Text } from '../../components/ui/text';
+import products from '../../assets/products.json';
+import { Card } from '../../components/ui/card';
+import { Image } from '../../components/ui/image';
+import { Box } from '../../components/ui/box';
+import { Button } from '../../components/ui/button';
+import { Heading } from '../../components/ui/heading';
+import { VStack } from '../../components/ui/vstack';
+import { ButtonText } from '../../components/ui/button';
 
 
 export default function ProductDetailsScreen() {
 
-    const {id} = useLocalSearchParams();
+    const { id } = useLocalSearchParams<{ id: string}>();
+
+    const product = products.find((p) => p.id === Number(id));
+
+    if (!product) {
+        return <Text>Product not found</Text>;
+    }
+
     return (
-
-
-            <Text>Product Details {id}</Text>
+        <Card className="p-5 rounded-lg max-w-[660px] flex-1">
+        <Image
+          source={{ uri: product.image, width: 300, height: 300 }}
+          className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+          alt={'${product.name}image'}
+          resizeMode='contain'
+        />
+        <Text className="text-sm font-normal mb-2 text-typography-700">
+          {product.name}
+        </Text>
+        <VStack className="mb-6">
+          <Heading size="md" className="mb-4">
+            {product.description}
+          </Heading>
+          <Text className="text-sm font-normal text-typography-700">
+            ${product.price}
+          </Text>
+        </VStack>
+        <Box className="flex-col sm:flex-row">
+          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+            <ButtonText size="sm">Add to cart</ButtonText>
+          </Button>
+          <Button
+            variant="outline"
+            className="px-4 py-2 border-outline-300 sm:flex-1"
+          >
+            <ButtonText size="sm" className="text-typography-600">
+              Wishlist
+            </ButtonText>
+          </Button>
+        </Box>
+      </Card>
 
     );
 }   
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,50 +1,33 @@
 
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
-import { Card} from './ui/card';
+import { Card } from './ui/card';
 import { Image } from './ui/image';
-import { Box } from './ui/box';
-import { Button } from './ui/button';
-import { Heading } from './ui/heading';
-import { VStack } from './ui/vstack'; 
-import { ButtonText } from './ui/button';
+import { Link } from 'expo-router';
 
 
 
 
-export const ProductListItem = ({product}   : {product: any}) => {
-    return (
+export const ProductListItem = ({ product }: { product: any }) => {
+  return (
+
+    <Link href={`/product/${product.id}`} asChild>
+      <Pressable className="flex-1">
         <Card className="p-5 rounded-lg max-w-[360px] flex-1">
-      <Image
-      source={{uri:product.image, width: 300, height: 300}}
-        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
-        alt={'${product.name}image'}
-        resizeMode='contain'
-      />
-      <Text className="text-sm font-normal mb-2 text-typography-700">
-        {product.name}
-      </Text>
-      <VStack className="mb-6">
-        <Heading size="md" className="mb-4">
-           {product.description}
-        </Heading>
-        <Text className="text-sm font-normal text-typography-700">
+          <Image
+            source={{ uri: product.image, width: 300, height: 300 }}
+            className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+            alt={'${product.name}image'}
+            resizeMode='contain'
+          />
+          <Text className="text-sm font-normal mb-2 text-typography-700">
+            {product.name}
+          </Text>
+          <Text className="text-sm font-normal text-typography-700">
             ${product.price}
-        </Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
-    </Card>
-    )
+          </Text>
+        </Card>
+      </Pressable>
+    </Link>
+  )
 }
